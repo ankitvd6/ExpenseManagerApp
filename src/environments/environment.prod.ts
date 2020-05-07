254c4270a0cc82bc801e25c57a96498559641ae0
Ankit Dwivedi <ankitvd6@gmail.com>
2020-05-07 18:01:59 +0530

    Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
   production: true,
-  firebase : {
-    apiKey: "AIzaSyDvsYB8K4MhGJXbmk8WIgnsfpYujR2RFl0",
-    authDomain: "expensemanager2-8d5bc.firebaseapp.com",
-    databaseURL: "https://expensemanager2-8d5bc.firebaseio.com",
-    projectId: "expensemanager2-8d5bc",
-    storageBucket: "expensemanager2-8d5bc.appspot.com",
-    messagingSenderId: "810240195268",
-    appId: "1:810240195268:web:86d8f7c71119c2df8b6791",
-    measurementId: "G-4DC7N3M4S0",
-  }
 };
